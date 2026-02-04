62c6b8aa537bc2de6e7479f63e7b39cc0fdfa160
AMN-47 <akhilnagar57@gmail.com>
2026-02-04 06:29:40 +0200

    Added if statement that handles roman numeral cases such as IV for 4 and IX for 9

--- Converter.py
+++ Converter.py
@@ -3,6 +3,25 @@ numeral_input = input("Enter the roman numerals you want to convert: ") #Input m
 
 def roman_to_int(numeral):#function responsible for converting Roman Numeral to Integer
     final_answer = 0
+    if "CM" in numeral:#These if statments handle cases for numbers such as IV for four, otherwise, it will read as six for example
+        final_answer += 900
+        numeral = numeral.replace("CM","")
+    if "CD" in numeral:
+        final_answer += 400
+        numeral = numeral.replace("CD","")
+    if "XC" in numeral:
+        final_answer += 90
+        numeral = numeral.replace("XC","")
+    if "XL" in numeral:
+        final_answer += 40
+        numeral = numeral.replace("XL","")
+    if "IX" in numeral:
+        final_answer += 9
+        numeral = numeral.replace("IX","")
+    if "IV" in numeral:
+        final_answer += 4
+        numeral = numeral.replace("IV","")
+    
     for i in numeral:
         if i == "M":
             final_answer += 1000#Adds 1000 to value if M is in input
